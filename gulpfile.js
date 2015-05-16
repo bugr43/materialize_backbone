23169879418c065875b44f0d77bff2ba26eb4879
bugr43 <bugr43@gmail.com>
2015-05-16 15:10:48 +0300

    Add gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
 
-gulp.task('default', function(){
-
+gulp.task('connect', function () {
+    connect.server({
+        port: 8080,
+        livereload: true
+    });
 });
+
+gulp.task('html', function () {
+    gulp.src('*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+    gulp.src('css/*.css')
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('*.html', ['html']);
+    gulp.watch('css/*.html', ['css'])
+});
+
+gulp.task('default', ['html', 'css', 'connect', 'watch']);
